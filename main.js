642307b2d01f84ece2b35ed28a9c8f3735691730
Kenyat1989 <erdkunde.1989@mapconcierge.com>
2016-03-03 17:06:57 +0900

    視点の設定

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-(function() {
-  "use strict";
-  var viewer = new Cesium.Viewer("cesium");
-}());
+var viewer = new Cesium.Viewer("cesium");
+
+// 初期の視点設定
+viewer.camera.setView({
+  destination: Cesium.Cartesian3.fromDegrees(139.6721684, 35.7052493, 50000),
+  orientation: {
+    heading: 0, // 水平方向の回転
+    pitch: -Cesium.Math.PI_OVER_TWO, // 垂直方向の回転 上を見上げたり下を見下ろしたり
+    roll: 0
+  }
+});
+
+var terrainProvider = new Cesium.JapanGSITerrainProvider({});
+viewer.terrainProvider = terrainProvider;
